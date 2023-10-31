91caf54dfb196fcf3d3d6efea956c9810d95151f
yevhen.stadnik <yevhen.stadnik@ihsmarkit.com>
2023-10-31 20:30:34 +0200

    Append multiple more solutions - done.

---
+++ SingleNumber.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class SingleNumber {
+
+    public static void main(String[] args) {
+        int[] n1 = {2,2,1};
+        int[] n4 = {4,1,2,1,2};
+        int[] n11 = {1};
+        System.out.println("The n1 array contains " + singleNumber2(n1) + " unique number.");
+        System.out.println("The n4 array contains " + singleNumber2(n4) + " unique number.");
+        System.out.println("The n11 array contains " + singleNumber2(n11) + " unique number.");
+    }
+
+    public static int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+
+    public static int singleNumber2(int[] nums) {
+        List<Integer> tmp = new ArrayList<>();
+        for (int num : nums) {
+            tmp.add(num);
+        }
+        for (int num : nums) {
+            if(tmp.indexOf(num) == tmp.lastIndexOf(num)) {
+                return num;
+            }
+        }
+        return 0;
+    }
+}
---
+++ ValidNumber.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+public class ValidNumber {
+
+    public static void main(String[] args) {
+        List<String> validNumbers = List.of("2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789");
+        List<String> invalidNumbers = List.of("abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53", "Infinity", "959440.94f");
+        validNumbers.stream().map(ValidNumber::isNumber).forEach(System.out::println);
+        System.out.println();
+        invalidNumbers.stream().map(ValidNumber::isNumber).forEach(System.out::println);
+    }
+
+    public static boolean isNumber(String s) {
+        try {
+            Double.parseDouble(s);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean isNumber2(String s) {
+        if(s.matches("[+-]?((\\d+)|((\\d+\\.)|(\\d+\\.\\d+)|(\\.\\d+)))([eE][+-]?\\d+)?")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
